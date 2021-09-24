b2542507bc6a836736ce721839dd9e352d2a528c
Alexandra Brown <alexabr@sas.upenn.edu>
2021-09-24 16:43:41 -0700

    Function to make bar charts where each bar shows the distribution of categories out of 100%. (All bars are the same height, and are colored by % of them that belongs to each category.)

--- sandbox/plotting_functions.R
+++ sandbox/plotting_functions.R
@@ -46,3 +46,51 @@ stackedHist <- function(data, colors=NULL, legend=NULL, legendLocation=NULL, plo
     legend(legendLocation, legend, pt.bg=colors, col="black", pch=22)
   }
 }
+
+
+# A function for plotting the distribution of different categories vs a dependent variable
+# This function basically makes a stacked histogram and then normalizes the heights of the histogram
+# bars so they are all the same. This is really useful for showing how the frequency of categories changes
+# as a variable changes. BUT it can also be misleading if you are trying to get a regular stacked histogram,
+# as it'll get rid of any variation in bar height. USE WITH CAUTION!!!
+# data should be a list of the form (data1=data to plot 1st, data2=data to plot 2nd, ...)
+evenBarsHist <- function(data, colors=NULL, legend=NULL, legendLocation=NULL, plotLegend=FALSE, breaks="Sturges", ...) {
+  plotargs <- list(...)
+
+  # Assign colors as shades of gray if no colors are given
+  if (is.null(colors)) {
+    colors <- lapply(seq(0, 0.9, length.out=length(data)), function(x) {rgb(x, x, x)})
+  }
+
+  # Create but don't plot a histogram of all the data
+  # This will be used get the breaks for the histograms we "stack" and to normalize their heights
+  histAll <- hist(unlist(data), breaks=breaks, plot=FALSE)
+  breaks <- histAll$breaks # get the breaks to use for the other histograms
+  normalizeBy <- histAll$counts # Get the total number of simulations in each bin to normalize by
+
+  # Plot the data as a stacked histogram.
+  # We go through the data backwards, first plotting a histogram of everything, then
+  # removing one dataset at a time and adding new histogram on top the other histograms
+  # This creates a "stacked" effect, with the first dataset at the bottom of the stack
+  histAll$density <- histAll$counts / normalizeBy * 100
+  do.call(plot, c(list(histAll, freq=FALSE, col=colors[[length(data)]]), plotargs))
+
+  for (i in (length(data)-1):1) {
+    histi <- hist(unlist(data[1:i]), breaks=breaks, plot=FALSE)
+    histi$density <- histi$counts / normalizeBy * 100
+    plot(histi, freq=FALSE, add=TRUE, col=colors[[i]])
+  }
+
+  # Add a legend
+  if (plotLegend) {
+    if (is.null(legend)) {
+      legend <- lapply(seq(1, length(data)), function(x) {paste0("data ", x)})
+    }
+
+    if (is.null(legendLocation)) {
+      legendLocation <- "topright"
+    }
+
+    legend(legendLocation, legend, pt.bg=colors, col="black", pch=22)
+  }
+}
